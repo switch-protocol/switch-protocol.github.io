806cf973d17492a41b2cd4286cc805ae50d24bc3
switch-protocol <switch.protocol.music@gmail.com>
2024-10-08 17:33:18 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*[]/:_-";
+const h1 = document.querySelector("h1");
 
 let interval = null;
+
+window.onload = function() {
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    h1.innerText = h1.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return h1.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= h1.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 40);
+};
+
 
 document.querySelector("h1").onmouseover = event => {  
   let iteration = 0;
@@ -24,5 +51,5 @@ document.querySelector("h1").onmouseover = event => {
     }
     
     iteration += 1 / 3;
-  }, 50);
+  }, 40);
 };
