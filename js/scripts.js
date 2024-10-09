99044e5852cde6020c1a30759a8b24e1a80e6635
switch-protocol <switch.protocol.music@gmail.com>
2024-10-09 04:14:01 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,23 +13,23 @@ window.onload = function() {
       .split("")
       .map((letter, index) => {
         if(index < iteration) {
-          return h1.target.dataset.value[index];
+          return h1.dataset.value[index];
         }
       
         return letters[Math.floor(Math.random() * 50)]
       })
       .join("");
     
-    if(iteration >= h1.target.dataset.value.length){ 
+    if(iteration >= h1.dataset.value.length){ 
       clearInterval(interval);
     }
     
     iteration += 1 / 3;
-  }, 40);
+  }, 30);
 };
 
 
-document.querySelector("h1").onmouseover = event => {  
+h1.onmouseover = event => {  
   let iteration = 0;
   
   clearInterval(interval);
@@ -51,5 +51,5 @@ document.querySelector("h1").onmouseover = event => {
     }
     
     iteration += 1 / 3;
-  }, 40);
+  }, 30);
 };
