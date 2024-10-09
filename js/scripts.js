8bf4e7d4dffdcb3d37d4aa14274b525ba51779b0
switch-protocol <switch.protocol.music@gmail.com>
2024-10-09 13:21:08 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -48,7 +48,7 @@ function animateText(target, duration = 1500, intervalTime = 35) {
 // Start animation on page load
 window.onload = function () {
   animateText(h1,1500,35);  // Initial load animation
-  animateText(h2,1500,35);
+  setTimeout(() => animateText(h2,1500,35), index * 200);
   /* Animate all h2 elements
   h2Headings.forEach((h2, index) => {
     setTimeout(() => animateText(h2, 0, 35), index * 200);  // Delay between each h2 animation
