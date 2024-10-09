ae53c1f6170ebf1eb6cb08d430d74a5e98343964
switch-protocol <switch.protocol.music@gmail.com>
2024-10-09 07:37:33 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*[]/:_-";
 const h1 = document.querySelector("h1");
-// const h2 = document.querySelector("h2");
-// const h3 = document.querySelector("h3");
 
 let interval = null;
 
@@ -49,8 +47,6 @@ function animateText(target, duration = 1500, intervalTime = 35) {
 // Start animation on page load
 window.onload = function () {
   animateText(h1,1500,35);  // Initial load animation
-  // animateText(h2,1500,35);  // Initial load animation
-  // animateText(h3,1500,35);  // Initial load animation
 };
 
 // Mouseover event to reset and restart animation
