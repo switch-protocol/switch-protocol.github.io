f2d6361ddc8de7cb0ae3f11dd4feb201a2cef271
switch-protocol <switch.protocol.music@gmail.com>
2024-10-09 14:12:05 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -44,17 +44,17 @@ function animateText(target, duration = 1500, intervalTime = 35, charTurns = 5) 
 window.onload = function () {
     const h1Elements = document.querySelectorAll("h1");
     h1Elements.forEach((h1) => {
-        intervals[h1.dataset.value] = animateText(h1, 1000, 35, 5);
+        intervals[h1.dataset.value] = animateText(h1, 1000, 35, 4);
     });
 
     const h2Elements = document.querySelectorAll("h2");
     h2Elements.forEach((h2) => {
-        intervals[h2.dataset.value] = animateText(h2, 2000, 35, 4);
+        intervals[h2.dataset.value] = animateText(h2, 2500, 35, 3);
     });
 
     const h3Elements = document.querySelectorAll("h3");
     h3Elements.forEach((h3) => {
-        intervals[h3.dataset.value] = animateText(h3, 3500, 35, 3);
+        intervals[h3.dataset.value] = animateText(h3, 3500, 35, 2);
     });
 };
 
