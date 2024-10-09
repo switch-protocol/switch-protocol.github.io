27a0e17f8b731e5adab60623ba68ea707fc8f452
switch-protocol <switch.protocol.music@gmail.com>
2024-10-09 06:38:39 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -39,14 +39,14 @@ function animateText(target, duration = 2000, intervalTime = 40) {
 
     // Update iteration as time progresses
     if (elapsedTime >= duration) {
-      iteration += 1 / 10;
+      iteration += 1 / 8;
     }
   }, intervalTime);
 }
 
 // Start animation on page load
 window.onload = function () {
-  animateText(h1,5000,40);  // Initial load animation
+  animateText(h1,2500,40);  // Initial load animation
 };
 
 // Mouseover event to reset and restart animation
