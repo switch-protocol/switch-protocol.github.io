2348ed21f2010eac6eee50007dc60bbe60ef955b
switch-protocol <switch.protocol.music@gmail.com>
2024-10-10 09:02:12 -0500

    Update links functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
+// Store the links in an object
+const links = {
+    artist_one_social: "https://soundcloud.com/keefemusic",
+    artist_two_social: "https://soundcloud.com/nathaniel_pavel",
+    artist_three_social: "https://soundcloud.com/mikext",
+    artist_one_sound: "https://soundcloud.com/keefemusic",
+    artist_two_sound: "https://soundcloud.com/nathaniel_pavel",
+    artist_three_sound: "https://soundcloud.com/mikext",
+    location_link: "https://www.google.com/maps/place/2011+W+North+Ave,+Chicago,+IL+60647,+USA/@41.9102697,-87.6786115,19z/data=!3m1!4b1!4m6!3m5!1s0x880fd2b8a7224829:0x6de43eb4ecc04f3c!8m2!3d41.9102687!4d-87.6779678!16s%2Fg%2F11b8z17mmr?entry=ttu&g_ep=EgoyMDI0MTAwNy4xIKXMDSoASAFQAw%3D%3D",
+    calendar_link: "https://calendar.google.com/calendar/share?slt=1AUWfa3gsRAQSLAmE-NqxPo_2UZuHul4EJiTR0JdQI5N0AtkIiRuWVwoXuqMkSuxqcaWoadotfRzBZQ",
+    ra_link: "https://ra.co/events/2009773",
+    instagram_link: "https://www.instagram.com/switch_protocol_/"
+};
 
-
-
+// Find all links with the class 'link' and dynamically set href from links object
+document.querySelectorAll('.link').forEach(function(anchor) {
+    // Get the URL reference from the data-url attribute
+    const urlKey = anchor.getAttribute('data-url');
+  
+    // Set the href of the link dynamically using the links object
+    if (links[urlKey]) {
+      anchor.href = links[urlKey];
+    }
+});
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*[]/:_-";
 
@@ -30,14 +51,9 @@ function animateText(target, duration = 1500, intervalTime = 35, charTurns = 5) 
             return `<span>${letters[Math.floor(Math.random() * letters.length)]}</span>`;
         }).join("");
 
-        // If all letters are revealed, clear the interval and make text a hyperlink
+        // If all letters are revealed, clear the interval
         if (iteration >= targetText.length) {
             clearInterval(interval);
-
-            // Wrap text in a link if a URL is provided in the data-url attribute
-            if (target.dataset.url) {
-                target.innerHTML = `<a href="${target.dataset.url}" target="_blank">${targetText}</a>`;
-            }
         }
 
         // Increase iteration as time progresses
@@ -68,7 +84,7 @@ window.onload = function () {
 };
 
 // Mouseover event to reset and restart animation on each element
-function restartAnimationOnHover(element, tag) {
+function restartAnimationOnHover(element) {
     element.addEventListener('mouseover', () => {
         // Clear the specific interval for the element
         if (intervals[element.dataset.value]) {
@@ -81,5 +97,3 @@ function restartAnimationOnHover(element, tag) {
 
 // Add the mouseover event listeners for all h1, h2, and h3 elements
 document.querySelectorAll("h1").forEach(restartAnimationOnHover);
-
-
